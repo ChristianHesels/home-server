160977e3c0f625f8361359c2694bfcf8a2a80e12
Christian Hesels <christianhesels@macbookchris.speedport.ip>
2022-10-02 10:31:05 +0200

    Added responsive mobile AppBar

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -3,11 +3,16 @@ import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import {ThemeProvider, createTheme} from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Link from 'next/link';
+import {Drawer, List, ListItem, ListItemText} from '@mui/material';
 
-const navItems = ['Home', 'About', 'Contact'];
+const navItems = [
+  {name: 'Home', link: '/'},
+  {name: 'VPN', link: '/vpn'},
+  {name: 'IP-Tables', link: '/iptable'},
+];
 
 export default function AppBar() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -31,11 +36,24 @@ export default function AppBar() {
           </IconButton>
           <Box sx={{display: {xs: 'none', sm: 'block'}}}>
             {navItems.map(item => (
-              <Button key={item} sx={{color: '#fff'}}>
-                {item}
-              </Button>
+              <Link href={item.link}>
+                <Button key={item.name} sx={{color: '#fff'}}>
+                  {item.name}
+                </Button>
+              </Link>
             ))}
           </Box>
+          <Drawer open={mobileOpen} onClose={() => setMobileOpen(false)}>
+            <List>
+              {navItems.map(item => (
+                <Link href={item.link}>
+                  <ListItem button>
+                    <ListItemText>{item.name}</ListItemText>
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </Drawer>
         </Toolbar>
       </MuiAppBar>
     </Box>
