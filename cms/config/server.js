0a1498dde18d6020d883bee3a3f8e6845c9e02e4
Christian Hesels <christianhesels@gmail.com>
2022-10-04 16:07:04 +0200

    Added Strapi CMS Folder for deployment

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e770fae16e53bf2abfe42c00124885a7'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6be054bf-3bc1-413b-81cf-3862b74737b4'
+};
