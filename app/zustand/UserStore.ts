4322c9c5340335b703113f4b35f43cdcb584008b
Christian Hesels <christianhesels@gmail.com>
2022-10-07 14:51:59 +0200

    Handled linting warning

--- app/zustand/UserStore.ts
+++ app/zustand/UserStore.ts
@@ -1,13 +1,13 @@
 import create from 'zustand';
 import {persist} from 'zustand/middleware';
 import {UserStore} from '../interfaces/userstore';
-import {User} from '../interfaces/user';
+import {UserLoginResponse} from '../interfaces/user';
 
 const useUserStore = create(
   persist<UserStore>(
     set => ({
       user: null,
-      setUser: (user: User | null) => set({user: user}),
+      setUser: (user: UserLoginResponse | null) => set({user: user}),
     }),
     {
       name: 'user-storage',
